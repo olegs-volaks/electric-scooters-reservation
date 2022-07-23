078a8b1e49b201c305c123c6405f179e6f1bdcd0
Olegs Volaks <olegs.volaks@gmail.com>
2022-07-23 22:53:57 +0300

    Added from date validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,7 @@ class Reservation < ApplicationRecord
 
   validates :from, :to, presence: true, :overlap => { scope: :scooter_id }
   validates :to, comparison: { greater_than: :from }
+  validates :from, comparison: { greater_than: DateTime.now }
 
   private
 
